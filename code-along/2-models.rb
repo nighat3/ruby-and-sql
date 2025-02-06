7cd7a1ec5d1f58a19efc0d601fb6dba82c1b5287
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-02-06 02:47:53 +0000

    class 5

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -11,17 +11,76 @@ Company.destroy_all
 # - Insert, read, update, and delete rows in companies table
 #   (i.e. full CRUD of company data).
 
+puts "hi"
+
 # 1a. check out the schema file
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
+companies = Company.all.count
+
+
+new_company = Company.new
+
+
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "apple.com"
+new_company.save
+puts new_company.inspect
+
+
+new_company = Company.new
+
+
+new_company["name"] = "Amazon"
+new_company["city"] = "Seattle"
+new_company["state"] = "WA"
+new_company.save
+puts new_company.inspect
+
+new_company = Company.new
+
+
+new_company["name"] = "Google"
+new_company["city"] = "Mountain View"
+new_company["state"] = "CA"
+new_company["url"] = "google.com"
+new_company.save
+puts new_company.inspect
+
+
+
+puts "There are #{Company.all.count} companies"
+
 
 # 3. query companies table to find all row with California company
 
+cali_companies = Company.where({"state" => "CA"})
+puts cali_companies.inspect
+
+
 # 4. query companies table to find single row for Apple
 
+apple = Company.where({"name" => "Apple"})[0]
+apple = Company.find_by({"name" => "Apple"})
+puts apple.inspect
+
+
 # 5. read a row's column value
+apple = Company.find_by({"name" => "Apple"})
+puts apple.inspect
 
 # 6. update a row's column value
+amazon = Company.find_by({"name" => "Amazon"})
+amazon["url"] = "amazon.com"
+amazon.save
+puts amazon.inspect
 
 # 7. delete a row
+
+google = Company.find_by({"name" => "Google"})
+google.destroy
+
+puts "There are #{Company.all.count} companies"
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,15 +21,58 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+salespeople = Salesperson.all.count
+
+
+new_salesperson = Salesperson.new
+
+
+new_salesperson["first_name"] = "Boo"
+new_salesperson["last_name"] = "Radley"
+new_salesperson["email"] = "boo@gmail.com"
+new_salesperson.save
+puts new_salesperson.inspect
+
+
+new_company = Company.new
+
+new_salesperson = Salesperson.new
+
+
+new_salesperson["first_name"] = "Atticus"
+new_salesperson["last_name"] = "Finch"
+new_salesperson["email"] = "afinch@gmail.com"
+new_salesperson.save
+puts new_salesperson.inspect
+
+
 # 3. write code to display how many salespeople rows are in the database
+
+puts "There are #{Salesperson.all.count} salespeople"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
 
+boo_data = Salesperson.find_by({"first_name" => "Boo", "last_name" => "Radley"})
+boo_data["email"] = "boor@gmail.com"
+boo_data.save
+puts boo_data.inspect
+
+
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+
+people = Salesperson.all
+for x in people
+
+first_name = x["first_name"]
+last_name = x["last_name"]
+name = "#{first_name} #{last_name}"
+puts name
+end
+
 
 # ---------------------------------
 # Salespeople: 2
